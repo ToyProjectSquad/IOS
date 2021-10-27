0c82cadfb4e5ec77809d5812faa9ff82389ff3b8
Jins_1312 <hjs7747@naver.com>
2021-10-27 22:29:30 +0900

    History - UI Elements

--- DailyCoffee/Views/MainViews/HistoryView.swift
+++ DailyCoffee/Views/MainViews/HistoryView.swift
@@ -6,13 +6,140 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct HistoryView: View {
+    
+    // MARK: - VARIABLES
+    @EnvironmentObject
+    var userVM: UserViewModel
+    @EnvironmentObject
+    var coffeeVM: CoffeeViewModel
+    
+    @State
+    private var mode: Int = 0
+    @State
+    private var durationOption: Int = 0
+    
+    // MARK: - BODY
     var body: some View {
         ZStack {
-            
+            Color("Background").ignoresSafeArea()
+            VStack(spacing: 0) {
+                Spacer().frame(maxHeight: 42)
+                titleView
+                pageControlView
+                    .padding([.leading, .trailing], 18)
+                    .padding(.bottom, 4)
+                contentView
+                    .padding(.bottom, 30)
+                commentView
+                Spacer()
+            }
         }
     }
+}
+
+// MARK: - COMPONENTS
+extension HistoryView {
+    
+    private var titleView: some View {
+        HStack {
+            Text("통계 기록")
+                .foregroundColor(Color("TextColor"))
+                .font(.system(size: 34, weight: .bold))
+                .padding(18)
+            Spacer()
+        }
+    }
+    
+    private var pageControlView: some View {
+        Picker("", selection: $mode) {
+            Text("차트")
+                .tag(0)
+            Text("캘린더")
+                .tag(1)
+        }
+        .pickerStyle(.segmented)
+        .background(Color(hex: "#E8E8E8"))
+        .cornerRadius(8)
+        .shadow(color: .black.opacity(0.4), radius: 5, x: 0, y: 5)
+    }
+    
+    private var contentView: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 15)
+                .foregroundColor(.white)
+            if mode == 0 { chartView }
+            else { calenderView }
+        }
+        .shadow(color: .black.opacity(0.4), radius: 5, x: 0, y: 5)
+        .padding([.leading, .trailing], 18)
+        .frame(height: 340)
+        
+    }
+    
+    private var commentView: some View {
+        VStack(alignment: .leading) {
+            Text("추가 정보")
+                .foregroundColor(Color("TextColor"))
+                .font(.system(size: 17, weight: .bold))
+                .padding(8)
+                .frame(maxWidth: .infinity, minHeight: 200, maxHeight: .infinity, alignment: .topLeading)
+        }
+        .background(RoundedRectangle(cornerRadius: 15).foregroundColor(.white))
+        .padding([.leading, .trailing], 18)
+    }
+    
+    private var chartView: some View {
+        VStack {
+            HStack {
+                Text(String(durationOption == 0 ? "주별" : "월별") + " 카페인")
+                    .font(.system(size: 24))
+                    .foregroundColor(Color("TextColor"))
+                    .padding()
+                Spacer()
+                Menu {
+                    Button {
+                        durationOption = 0
+                    } label: {
+                        Text("주별")
+                    }
+                    
+                    Button {
+                        durationOption = 1
+                    } label: {
+                        Text("월별")
+                    }
+
+                } label: {
+                    Image("Ellipsis")
+                        .padding()
+                }
+
+            }
+            Spacer()
+        }
+        
+    }
+    
+    private var calenderView: some View {
+        Text("캘린더 뷰")
+    }
+    
+    private var lineChart: some View {
+        Text("")
+//        LineView(data: [8,23,54,32,12,37,7,23,43], style: Styles.lineChartStyleOne)
+//            .border(.black)
+    }
+    
+    private var barChart: some View {
+        Text("")
+//        BarChartView(data: ChartData(values: [("Jan", 2), ("Feb", 4), ("Mar", 8), ("Apr", 3), ("May", 11), ("Jun", 7)]),
+//                     title: "Bar Chart", form: ChartForm.extraLarge)
+//            .border(.black)
+    }
+    
 }
 
 struct HistoryView_Previews: PreviewProvider {
